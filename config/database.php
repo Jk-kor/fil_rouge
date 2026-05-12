b224f51dcdc3ea0da171a3d1bd96a52e66ad2dbd
Jk-kor <ljk20301@naver.com>
2026-05-12 18:19:14 +0200

    Delete config/database.php

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Fichier : config/database.php
-return [
-    'host' => '127.0.0.1',
-    'dbname' => 'immobilier_db',
-    'user' => 'dev_user',
-    'pass' => 'jinq123123@',
-    'charset' => 'utf8mb4'
-];
